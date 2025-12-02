ef27a437e257743cad6f1d6c4b28c7d92d94dc2f
Gurwinder Singh <gurwinx369@gmail.com>
2025-12-02 06:24:17 +0530

    feat: add index file for models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+import UserProfileSchemaModel from "./user.profile.model.js";
+import SalonProfileSchemaModel from "./salon.profile.model.js";
+import ChatSchemaModel from "./chat.model.js";
+import SubscriptionSchemaModel from "./subscription.model.js";
+import BookingSchemaModel from "./booking.model.js";
+import NotificationSchemaModel from "./notification.model.js";
+
+export {
+  UserProfileSchemaModel,
+  SalonProfileSchemaModel,
+  ChatSchemaModel,
+  SubscriptionSchemaModel,
+  BookingSchemaModel,
+  NotificationSchemaModel,
+};
